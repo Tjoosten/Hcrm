7a864fdf440657c0732087cf92f0abb4f74c772a
Tjoosten <Tjoosten3@gmail.com>
2016-06-18 01:07:26 +0200

    Hcrm-16 WEB: add method to change user credentails.

---
+++ app/Http/Controllers/AccountController.php
@@ -0,0 +1,51 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\User;
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class AccountController extends Controller
+{
+    /**
+     * AccountController constructor.
+     */
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    /**
+     * Update the account information.
+     *
+     * @url    POST: /account/update/information
+     * @param  Requests\AccountValidator $input
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function updateAccountInformation(Requests\AccountValidator $input)
+    {
+        $id = auth()->user()->id;
+        User::find($id)->update($input->except('_token'));
+
+        session()->flash('message', 'Your account information has been updated');
+        return redirect()->back();
+    }
+
+    /**
+     * Update the security information about the account.
+     *
+     * @url    POST: /account/update/password
+     * @param  Requests\PasswordValidator $input
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function updateAccountSecurity(Requests\PasswordValidator $input)
+    {
+        $id = auth()->user()->id;
+        User::find($id)->update($input->except(['_token', 'password_confirmation']));
+
+        session()->flash('message', 'Your password has been updated');
+        return redirect()->back();
+    }
+}
