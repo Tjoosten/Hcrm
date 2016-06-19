44dc39c8b52888e391ebc28b2599a73437f9f67c
Glenn Hermans <glhermans@gmail.com>
2016-06-19 05:40:37 +0000

    Added language files

---
+++ resources/lang/en/billing.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+
+    'index' => 'Billing',    
+    'listTransactions' => 'List all Transactions',
+    'overdueInvoices' => 'Invoices overdue',
+    'quotes' => 'Quotes',
+
+
+
+];
---
+++ resources/lang/en/orders.php
@@ -0,0 +1,11 @@
+<?php
+
+return [
+
+    'index' => 'Orders',    
+    'register' => 'Add new order',
+    'list' => 'List all orders',
+
+    'products' => 'Products / Services',
+    'productGroups' => 'Product groups',    
+];
