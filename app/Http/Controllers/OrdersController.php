ff8fac99605c6ddf20275715e655f090be64147e
Tjoosten <Tjoosten3@gmail.com>
2016-06-19 23:35:24 +0200

    Add controller

---
+++ app/Http/Controllers/OrdersController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Http\Requests;
+
+class OrdersController extends Controller
+{
+    /**
+     * OrdersController constructor.
+     */
+    public function __construct()
+    {
+        $this->middleware('lang');
+        $this->middleware('auth');
+    }
+
+    public function newOrderView()
+    {
+        
+    }
+
+    public function postNewOrder()
+    {
+
+    }
+
+    public function allOrders()
+    {
+
+    }
+}
