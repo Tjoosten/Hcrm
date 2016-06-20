7d9132ceb3971c65255daf9e2a1c1d85c9c63652
Tjoosten <Tjoosten3@gmail.com>
2016-06-20 02:25:48 +0200

    Push dev view for tickets.

--- resources/views/tickets/index.blade.php
+++ resources/views/tickets/index.blade.php
@@ -4,9 +4,34 @@
     <div class="container">
         <div class="row">
             <div class="col-sm-9 col-xs-9 col-md-9 col-lg-9">
+                <div class="panel panel-default">
+                    <div class="panel-body">
+                        <table class="table table-hover table-condensed">
+                            <thead>
+
+                            </thead>
+                            <tbody>
+                                @foreach($tickets as $ticket)
+                                @endforeach
+                            </tbody>
+                        </table>
+
+                        {{ $tickets->render() }}
+                    </div>
+                </div>
             </div>
 
             <div class="col-sm-3 col-md-3 col-lg-3 col-xs-3">
+                <div class="panel panel-default">
+                    <div class="list-group">
+                        <a class="list-group-item" href="">
+                            All Tickets.
+                        </a>
+                        <a class="list-group-item" href="{!! route('Tickets.assigned') !!}">
+                            Assigned to you.
+                        </a>
+                    </div>
+                </div>
             </div>
         </div>
     </div>
