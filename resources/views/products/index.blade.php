0dfc354961d725453e27bb4cc9a9ff3c4b1b7268
Tjoosten <Tjoosten3@gmail.com>
2016-07-11 13:35:34 +0200

    Add new category modal

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -6,7 +6,7 @@
         <div class="row">
             <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
                 <a class="btn btn-default" role="button" data-toggle="collapse" href="#search" aria-expanded="false" aria-controls="collapseExample">Search products</a>
-                <a class="btn btn-primary" role="button" href="">New category</a>
+                <a class="btn btn-primary" role="button" href="" data-toggle="modal" data-target="#newCat">New category</a>
                 <a class="btn btn-success" role="button" href="">New product</a>
 
                 {{-- Search Well --}}
@@ -61,5 +61,53 @@
             </div>
         </div>
         {{-- End main content --}}
+
+        {{-- Models --}}
+
+            {{-- New category modal --}}
+            <div class="modal fade" id="newCat" tabindex="-1" role="dialog">
+                <div class="modal-dialog">
+                    <div class="modal-content">
+                        <div class="modal-header">
+                            <button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+                            <h4 class="modal-title">New category</h4>
+                        </div>
+                        <div class="modal-body">
+                            <form method="POST" action="" class="form-horizontal">
+                                {{-- CSRF token --}}
+                                {{ csrf_field() }}
+
+                                <div class="form-group">
+                                    <label class="col-sm-3 control-label" for="zipcode">
+                                        Category: <span class="text-danger">*</span>
+                                    </label>
+                                    <div class="col-sm-9">
+                                        <input value="" type="text" id="category" name="category" placeholder="Category name" class="form-control" />
+                                    </div>
+                                </div>
+
+                                <div class="form-group">
+                                    <label class="col-sm-3 control-label" for="description">
+                                        Description: <span class="text-danger">*</span>
+                                    </label>
+
+                                    <div class="col-sm-9">
+                                        <textarea name="description" class="form-control" placeholder="description" id="description" cols="30" rows="10"></textarea>
+                                    </div>
+                                </div>
+
+                        </div>
+                        <div class="modal-footer">
+                                <button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+                                <button type="reset" class="btn btn-warning">Reset</button>
+                                <button type="submit" class="btn btn-primary">Save</button>
+                            </form>
+                        </div>
+                    </div>{{-- /.modal-content --}}
+                </div>{{-- /.modal-dialog --}}
+            </div>{{-- /.modal --}}
+            {{-- End new category modal --}}
+
+        {{-- END modals --}}
     </div>
 @endsection
