2507241cccd5390898f04eb37f3a4c918f94fd5d
Tjoosten <Tjoosten3@gmail.com>
2016-07-21 22:41:56 +0200

    Add dedicated servers migration

---
+++ database/migrations/2016_07_21_203842_create_dedicated_servers_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDedicatedServersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('dedicated_servers', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('customer_id');
+            $table->string('server_name');
+            $table->string('reference');
+            $table->string('status');
+            $table->string('provider');
+            $table->integer('provider_id');
+            $table->string('location');
+            $table->string('diskspace');
+            $table->string('traffic');
+            $table->string('start_date');
+            $table->string('end_date');
+            $table->string('sla');
+            $table->string('vendor');
+            $table->string('vendor_product');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::drop('dedicated_servers');
+    }
+}
