80753ece716c400c209632464ba98ba80ba02ed0
Glenn Hermans <glhermans@gmail.com>
2016-07-21 07:08:58 +0200

    #121 - quick update

--- app/Http/Controllers/DedicatedServersController.php
+++ app/Http/Controllers/DedicatedServersController.php
@@ -11,9 +11,51 @@ class DedicatedServersController extends Controller
     /**
      * DedicatedServersController constructor.
      */
+
     public function __construct()
     {
         $this->middleware('auth');
         $this->middleware('lang');
     }
+
+    /**
+     * The index view for all the dedicated servers.
+     *
+     * @url    GET: /dedicatedservers
+     * @param  Request $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function index()
+
+    {
+    	return view('dedicatedservers.index');
+    }
+
+    /**
+     * The index view for all the dedicated servers.
+     *
+     * @url    GET: /dedicatedservers/register
+     * @param  Request $request
+     * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
+     */
+    public function register()
+
+    {
+    	return view('dedicatedservers.register');
+    }
+
+    /**
+     * Save the dedicated server data to the database.
+     *
+     * @url    POST: /dedicatedservers/save
+     * @param  Requests\DepartmentValidator $input
+     * @return \Illuminate\Http\RedirectResponse
+     */
+    public function store()
+
+    {
+
+
+    }
+            
 }
