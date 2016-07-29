2ef86bf3b7e4a856fa24f0677e0ba938d1eb9cee
Glenn Hermans <glhermans@gmail.com>
2016-07-29 09:32:28 +0200

    added dutch dedicatedServers file

---
+++ resources/lang/nl/dedicatedServers.php
@@ -0,0 +1,56 @@
+<?php
+
+return [
+
+    'title'             => 'Server',
+    // tabs
+    'overview'          => 'Overzicht',
+    'network'           => 'Netwerk',
+    'logs'              => 'Logs',
+    'hardware'          => 'Hardware details',
+    'software'          => 'Software',
+
+    'actions'           => 'Acties',
+    'createNote'        => 'Notitie maken',
+    'maintenance'       => 'Onderhoud',
+    'maintenanceStart'  => 'Start onderhoud',
+    'powerManagement'   => 'Stroombeheer',
+    'startProvisioning' => 'Provisionering opstarten',
+    'startReinstall'    => 'Herinstallatie starten',
+
+    'graphs'            => 'Graphs',
+    // overview
+    'overviewTitle'     => 'General overview',
+    'serverName'        => 'Server naam',
+    'customer'          => 'Customer',
+    'provider'          => 'Provider',
+    'cabinet'           => 'Cabinet',
+    'site'              => 'Site',
+    'serverStatus'      => 'Status',
+    'sla'               => 'SLA',
+    'dateCreated'       => 'Date aangemaakt',
+    'dateModified'      => 'Onlangs gewijzigd',
+    // IP details
+    'ipOverview'        => 'IP overzicht',
+    'ip'                => 'IP adres',
+    'ipType'            => 'Type',
+
+    'ptr'               => 'PTR',
+
+    // Reinstall
+    'ReinstallTitle'             => 'Server herinstalleren',
+    'ReinstallOs'                => 'Besturingssysteem',
+    'ReinstallOShelper'          => 'Selecteer het besturingssysteem dat u wilt installeren op deze machine',
+    'ReinstallDescription'       => 'Omschrijving',
+    'ReinstallDescriptionHelper' => 'Gelieve een reden in te vullen waarom deze server opnieuw geinstalleerd moet worden.',
+
+    'ReinstallDesc1'             => 'Het opstarten van een herinstallatie op deze machine is een destructief proces.',
+    'ReinstallDesc2'             => 'Wees zeker dat je dit wenst uit te voeren.',
+    'ReinstallDesc3'             => 'Dit process zal:',
+
+    'ReinstallDesc4'             => 'Controleren dat de server stabiel is:',
+    'ReinstallDesc5'             => 'Heropstarten:',
+    'ReinstallDesc6'             => 'Het besturingssysteem herinstalleren',
+    'ReinstallDesc7'             => 'Administrator gegevens vernieuwen',
+
+];
