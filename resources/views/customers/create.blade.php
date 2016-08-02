f532f61ae109e5648c0f0ae0af76425c60842e3d
Tjoosten <Tjoosten3@gmail.com>
2016-08-03 01:16:55 +0200

    remove @can statement

--- resources/views/customers/create.blade.php
+++ resources/views/customers/create.blade.php
@@ -1,6 +1,5 @@
 @extends('layouts.app')
 @section('content')
-@can('create-customers')
     <div class="row">
 <div class="container">
         <div class="col-sm-12 col-md-12 col-lg-12 col-xs-12">
@@ -211,13 +210,5 @@
             </form>
         </div>
     </div>
-    @else
-    <div class="container">
-    <div class="col-md-12">
-      <div class="alert alert-danger">
-       <p class="text-danger">You need permission to perform this action</p>
     </div>
-    </div>
-    </div>
-    @endcan
 @endsection
